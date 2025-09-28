af19e396fe320677a8bf86720c88cfa89551e063
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-09-28 15:18:04 +0400

    implement favicon update

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+
+export default layout;
